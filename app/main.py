6bf2deec6f52ca7b3815f29217bc4ca5a0f3569f
Melo <Vinicius.Melo1@br.nestle.com>
2019-03-18 23:28:37 -0300

    Feat: addition of models and functions views.

--- app/main.py
+++ app/main.py
@@ -1,13 +1,131 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import DevConfig
+from sqlalchemy import func
+
+import datetime
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/')
-def home():
-    return '<h1>Hello World!</h1>'
+@app.route('/<int:page>')
+def home(page=1):
+    posts = Post.query.order_by(Post.publish_date.desc()).paginate(
+        page,
+        app.config['POSTS_PER_PAGE'],
+        False
+    )
+    recent, top_tags = sidebar_data()
+
+    return render_template(
+        'home.html',
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
+
+
+tags = db.Table(
+    'post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    pass
+
+
+@app.route('/posts_by_user/<string:username>')
+def posts_by_user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data()
+
+    return render_template(
+        'user.html',
+        user=user,
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255), nullable=False, unique=True)
+    password = db.Column(db.String(255))
+    posts = db.relationship(
+        'Post',
+        backref='user',
+    )
+
+    def __init__(self, username):
+        self.username = username
+
+    def __repr__(self):
+        return "<User '{}'>".format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    comments = db.relationship(
+        'Comment',
+        backref='post',
+    )
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255), nullable=False, unique=True)
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+
+def sidebar_data():
+    recent = Post.query.order_by(
+        Post.publish_date.desc()
+    ).limit(5).all()
+
+    top_tags = db.session.query(
+        Tag, func.count(tags.c.post_id).label('total')
+    ).join(
+        tags
+    ).group_by(Tag).order_by('total DESC').limit(5).all()
+
+    return recent, top_tags
 
 
 if __name__ == "__main__":
