362d3e4201ca43a7c8483189b4a0e213d6e46a7b
Melo <Vinicius.Melo1@br.nestle.com>
2019-03-20 23:49:10 -0300

    Feat: Flask Blog finished (1st Part), functional blog working smoothly.

--- app/config.py
+++ app/config.py
@@ -3,10 +3,11 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
+    SECRET_KEY = '\xa8\xcc\xeaP+\xb3\xe8|\xad\xdb\xea\xd0\xd4\xe8\xac\xee\xfaW\x072@O3'
 
 
 class DevConfig(Config):
     degug = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
     SQLALCHEMY_ECHO = True
+    SECRET_KEY = '\xa8\xcc\xeaP+\xb3\xe8|\xad\xdb\xea\xd0\xd4\xe8\xac\xee\xfaW\x072@O3'
--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+import datetime
+
+from flask import Flask, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import DevConfig
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Length
 from sqlalchemy import func
-
-import datetime
+from config import DevConfig
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
@@ -12,14 +15,93 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 
+tags = db.Table(
+    'post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
+)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(255))
+    password = db.Column(db.String(255))
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __init__(self, username=""):
+        self.username = username
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime())
+    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
+    comments = db.relationship(
+        'Comment',
+        backref='post',
+        lazy='dynamic'
+    )
+    tags = db.relationship(
+        'Tag',
+        secondary=tags,
+        backref=db.backref('posts', lazy='dynamic')
+    )
+
+    def __init__(self, title=""):
+        self.title = title
+
+    def __repr__(self):
+        return "<Post '{}'>".format(self.title)
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    date = db.Column(db.DateTime(), default=datetime.datetime.now)
+    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
+
+    def __repr__(self):
+        return "<Comment '{}'>".format(self.text[:15])
+
+
+class Tag(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(255))
+
+    def __init__(self, title=""):
+        self.title = title
+
+    def __repr__(self):
+        return "<Tag '{}'>".format(self.title)
+
+
+class CommentForm(Form):
+    name = StringField(
+        'Name',
+        validators=[DataRequired(), Length(max=255)]
+    )
+    text = TextAreaField(u'Comment', validators=[DataRequired()])
+
+
+def sidebar_data():
+    recent = Post.query.order_by(Post.publish_date.desc()).limit(5).all()
+    top_tags = db.session.query(
+        Tag, func.count(tags.c.post_id).label('total')
+    ).join(tags).group_by(Tag).order_by('total DESC').limit(5).all()
+
+    return recent, top_tags
+
+
 @app.route('/')
 @app.route('/<int:page>')
 def home(page=1):
-    posts = Post.query.order_by(Post.publish_date.desc()).paginate(
-        page,
-        app.config['POSTS_PER_PAGE'],
-        False
-    )
+    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, app.config.get('POSTS_PER_PAGE', 10), False)
     recent, top_tags = sidebar_data()
 
     return render_template(
@@ -30,17 +112,53 @@ def home(page=1):
     )
 
 
-tags = db.Table(
-    'post_tags',
-    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'))
-)
-
-
-@app.route('/post/<int:post_id>')
+@app.route('/post/<int:post_id>', methods=('GET', 'POST'))
 def post(post_id):
+    form = CommentForm()
+    if form.validate_on_submit():
+        new_comment = Comment()
+        new_comment.name = form.name.data
+        new_comment.text = form.text.data
+        new_comment.post_id = post_id
+        try:
+            db.session.add(new_comment)
+            db.session.commit()
+        except Exception as e:
+            flash('Error adding your comment: %s' % str(e), 'error')
+            db.session.rollback()
+        else:
+            flash('Comment added', 'info')
+        return redirect(url_for('post', post_id=post_id))
+
     post = Post.query.get_or_404(post_id)
-    pass
+    tags = post.tags
+    comments = post.comments.order_by(Comment.date.desc()).all()
+    recent, top_tags = sidebar_data()
+
+    return render_template(
+        'post.html',
+        post=post,
+        tags=tags,
+        comments=comments,
+        recent=recent,
+        top_tags=top_tags,
+        form=form
+    )
+
+
+@app.route('/posts_by_tag/<string:tag_name>')
+def posts_by_tag(tag_name):
+    tag = Tag.query.filter_by(title=tag_name).first_or_404()
+    posts = tag.posts.order_by(Post.publish_date.desc()).all()
+    recent, top_tags = sidebar_data()
+
+    return render_template(
+        'tag.html',
+        tag=tag,
+        posts=posts,
+        recent=recent,
+        top_tags=top_tags
+    )
 
 
 @app.route('/posts_by_user/<string:username>')
@@ -57,76 +175,5 @@ def posts_by_user(username):
         top_tags=top_tags
     )
 
-
-class User(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(255), nullable=False, unique=True)
-    password = db.Column(db.String(255))
-    posts = db.relationship(
-        'Post',
-        backref='user',
-    )
-
-    def __init__(self, username):
-        self.username = username
-
-    def __repr__(self):
-        return "<User '{}'>".format(self.username)
-
-
-class Post(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    text = db.Column(db.Text())
-    publish_date = db.Column(db.DateTime(), default=datetime.datetime.now)
-    user_id = db.Column(db.Integer(), db.ForeignKey('user.id'))
-    comments = db.relationship(
-        'Comment',
-        backref='post',
-    )
-
-    def __init__(self, title):
-        self.title = title
-
-    def __repr__(self):
-        return "<Post '{}'>".format(self.title)
-
-
-class Comment(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(255), nullable=False)
-    text = db.Column(db.Text())
-    date = db.Column(db.DateTime(), default=datetime.datetime.now)
-    post_id = db.Column(db.Integer(), db.ForeignKey('post.id'))
-
-    def __repr__(self):
-        return "<Comment '{}'>".format(self.text[:15])
-
-
-class Tag(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    title = db.Column(db.String(255), nullable=False, unique=True)
-
-    def __init__(self, title):
-        self.title = title
-
-    def __repr__(self):
-        return "<Tag '{}'>".format(self.title)
-
-
-def sidebar_data():
-    recent = Post.query.order_by(
-        Post.publish_date.desc()
-    ).limit(5).all()
-
-    top_tags = db.session.query(
-        Tag, func.count(tags.c.post_id).label('total')
-    ).join(
-        tags
-    ).group_by(Tag).order_by('total DESC').limit(5).all()
-
-    return recent, top_tags
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
